b922a7fc587d1c2a78ca87e974aea432412e9be6
Pandu Wicaksono <pandu.wicaksono91@gmail.com>
2017-06-07 15:43:15 +0200

    for normalized data

--- weka-experiment/wekaexperiment/src/main/java/edu/ufrt/wekaexperiment/wekaexperiment/App.java
+++ weka-experiment/wekaexperiment/src/main/java/edu/ufrt/wekaexperiment/wekaexperiment/App.java
@@ -24,6 +24,8 @@ public class App
 	
 	static String[][] evaluationData;
 	static String[][] coefficientData;
+	static String[][] testData;
+//	static String testFile = "";
 	
 	private static void checkAttributes(Instances instances){
 		System.out.println("Check attributes:");
@@ -35,14 +37,25 @@ public class App
 	
     public static void main( String[] args ) throws Exception
     {
+    	// header
     	evaluationData = new String[51][7];
-    	evaluationData[0][0] = "dataset name";
+    	evaluationData[0][0] = "train set name";
     	evaluationData[0][1] = "Total number of instances";
     	evaluationData[0][2] = "Correlation coefficient";
     	evaluationData[0][3] = "Mean absolute error";
     	evaluationData[0][4] = "Root mean squared error";
     	evaluationData[0][5] = "Relative absolute error";
     	evaluationData[0][6] = "Root relative squared error";
+    	
+    	testData = new String[1000][8];
+    	testData[0][0] = "train set name";
+    	testData[0][1] = "test set name";
+    	testData[0][2] = "Total number of instances";
+    	testData[0][3] = "Correlation coefficient";
+    	testData[0][4] = "Mean absolute error";
+    	testData[0][5] = "Root mean squared error";
+    	testData[0][6] = "Relative absolute error";
+    	testData[0][7] = "Root relative squared error";
     	
 //    	coefficientData = new String[51][11];
     	coefficientData = new String[51][12];
@@ -67,6 +80,8 @@ public class App
 
         printEvaluation();
         printCoefficient();
+        printTestData();
+        
     }
     
     private static void printEvaluation() throws FileNotFoundException{
@@ -74,6 +89,17 @@ public class App
     	for(int ii = 0; ii < evaluationData.length; ii ++){
     		for(int jj = 0; jj < evaluationData[ii].length; jj++){
     			writer.write(evaluationData[ii][jj] + ",");
+    		}
+    		writer.write("\n");
+    	}
+    	writer.close();
+    }
+    
+    private static void printTestData() throws FileNotFoundException{
+    	PrintWriter writer = new PrintWriter ("test result Martina.csv");
+    	for(int ii = 0; ii < testData.length; ii ++){
+    		for(int jj = 0; jj < testData[ii].length; jj++){
+    			writer.write(testData[ii][jj] + ",");
     		}
     		writer.write("\n");
     	}
@@ -128,7 +154,7 @@ public class App
 		}	
         
         train.setClassIndex(classIndex);
-        
+       
         Instances trainReduced = sourceTrain.getDataSet();
         trainReduced.setClassIndex(classIndex);
         
@@ -162,12 +188,17 @@ public class App
 //      System.out.println("Building Model Result:");
         PrintWriter printWriter = new PrintWriter(trainFile.split("\\.")[0] + " - model result.txt" );
         printWriter.write("helpfulness = \n");
+//        System.out.print("helpfulness = \n");
         for(int ii = 0; ii < lmCoeffs.length; ii++){
         	coefficientData[fileNumber][ii+1] = "" + lmCoeffs[ii];
-        	if(ii != lmCoeffs.length - 1)
+        	if(ii != lmCoeffs.length - 1){
+//        		System.out.print(lmCoeffs[ii] + " * " + trainReduced.attribute(ii).name() + " + \n");
         		printWriter.write(lmCoeffs[ii] + " * " + trainReduced.attribute(ii).name() + " + \n");
-        	else
+        	}
+        	else{
+//        		System.out.println("" + lmCoeffs[ii]);
         		printWriter.write("" + lmCoeffs[ii]);
+        	}
         }
         
         printWriter.close();
@@ -192,8 +223,106 @@ public class App
         evaluationData[fileNumber][6] = "" + evaluation.rootRelativeSquaredError();
         
 //        System.out.println(evaluation.toSummaryString("\nCross Validation Result", true));
-   
-    	
+        
+        // evaluate using test set
+        // initialize test data
+        
+        System.out.println("Train set: " + trainFile);
+        
+        File folderTest = new File(".\\");
+        File[] listOfFilesTest = folderTest.listFiles();
+        
+        LinkedList<String> fileNamesTest = new LinkedList<String>();
+        
+        for(int ii = 0; ii < listOfFilesTest.length; ii++){
+        	if(listOfFilesTest[ii].isFile()) {
+//        		System.out.println(listOfFilesTest[ii]);
+        		String fileName = listOfFilesTest[ii].getName();
+        		if(fileName.endsWith("arff"))
+        			fileNamesTest.add(fileName);
+        	}
+        }
+        
+        int nbTestFile = fileNamesTest.size();
+        
+        for(int ii = 0; ii < nbTestFile; ii++){
+        	System.out.println("Test Set: " + fileNamesTest.get(ii));
+        	String testFile = fileNamesTest.get(ii);
+        	DataSource sourceTest = new DataSource(testFile);
+	        Instances test;
+	        test = sourceTest.getDataSet();
+	        test.setClassIndex(classIndex);
+	        
+	        evaluation = new Evaluation(test);
+	        evaluation.evaluateModel(filteredClassifier, test);
+	//	        System.out.println(evaluation.toSummaryString("\nTest Set Result", true));
+	        int counter = (fileNumber-1)*nbTestFile+ii;
+//	        System.out.println("fileNumber: " + fileNumber + " counter: " + counter);
+	        testData[counter][0] = trainFile;
+	        testData[counter][1] = testFile;
+	        testData[counter][2] = "" + (int)evaluation.numInstances();
+	        testData[counter][3] = "" + evaluation.correlationCoefficient();
+	        testData[counter][4] = "" + evaluation.meanAbsoluteError();
+	        testData[counter][5] = "" + evaluation.rootMeanSquaredError();
+	        testData[counter][6] = "" + evaluation.relativeAbsoluteError();
+	        testData[counter][7] = "" + evaluation.rootRelativeSquaredError();
+        }   
+    }
+    
+    private static void generateKnowledgeFlowData(String filename) throws Exception{
+    	// initialize train data
+        DataSource sourceTrain = new DataSource(filename);
+        Instances train;
+        train = sourceTrain.getDataSet();
+        
+        // remove unused indices
+        int classIndex = 0;
+//        int[] removedIndices = new int [10];
+        int[] removedIndices = new int [11];
+        int j = 0;
+        
+        for (int i = 0; i < train.numAttributes(); i++){
+			String name = train.attribute(i).name();
+//			System.out.println("Check attribute no " + i + " : " + name);
+			if (name.startsWith("helpfulness")){
+				classIndex = i;
+//				System.out.println("Set class : " + name);
+			} else if (name.startsWith("asin") || 
+					  name.startsWith("reviewerID") || 
+					  name.startsWith("reviewDate") || 
+					  name.startsWith("nbHelpful") ||
+					  name.startsWith("nbVotes") ||
+					  name.startsWith("spellingErrRatio") ||
+					  name.startsWith("reviewTextFOG") ||
+					  name.startsWith("summaryFOG") ||
+					  name.startsWith("summaryFK") ||
+					  name.startsWith("reviewTextARI") ||
+					  name.startsWith("summaryARI")){
+				removedIndices[j] = i;
+				j++;
+//				System.out.println("Remove attribute: " + name);
+			} 
+		}	
+        
+        train.setClassIndex(classIndex);
+        
+        Instances trainReduced = sourceTrain.getDataSet();
+        trainReduced.setClassIndex(classIndex);
+        
+        Remove remove = new Remove();
+        remove.setAttributeIndicesArray(removedIndices);
+        remove.setInputFormat(train);
+        
+        // create reduced instances
+        trainReduced = Filter.useFilter(trainReduced, remove);
+//        checkAttributes(trainReduced);
+        
+        // create linear regression
+        LinearRegression linearRegression = new LinearRegression();
+        linearRegression.setNumDecimalPlaces(8);
+        linearRegression.buildClassifier(trainReduced);
+        
+        
     }
     
 }
